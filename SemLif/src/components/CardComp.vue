41c15785808473fbe2c6ef8296c241c6d39205ad
KaueDeMoura <kaueoenningdmoura@gmail.com>
2022-10-10 14:21:12 -0300

    teste

--- SemLif/src/components/CardComp.vue
+++ SemLif/src/components/CardComp.vue
@@ -15,18 +15,21 @@ export default {
     mostrarFilme() {
       alert(this.filme.autor);
     },
+    getImageUrl(poster_path) {
+      return `https://image.tmdb.org/t/p/w500/${poster_path}`;
+    },
   },
 };
 </script>
 
 <template>
   <div id="card1" class="ml-5 mr-5">
-    <div class="absolute z-10 text-white">aaaaaaaaaaa</div>
+    <div class="absolute z-10 text-white">{{ filme.title }}</div>
     <div class="rounded-lg shadow-lg w-64">
       <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
         <img
           class="rounded-t-lg"
-          src="https://mdbootstrap.com/img/new/standard/nature/182.jpg"
+          :src="getImageUrl(filme.poster_path)"
           alt=""
         />
       </a>
--- SemLif/src/views/FilmesView.vue
+++ SemLif/src/views/FilmesView.vue
@@ -1,10 +1,13 @@
 <script>
+import axios from "axios ";
 import CardComp from "../components/CardComp.vue";
 
 export default {
   components: { CardComp },
   data() {
     return {
+      generos: [],
+      filmesPorGenero: [],
       movies: [
         {
           id: 1,
@@ -23,29 +26,52 @@ export default {
         },
         {
           id: 6,
-        }
+        },
       ],
     };
+  },
+  async created() {
+    const { data } = await axios.get(
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=cdd74c9f441352e6a6aee5e2cc6532e6&language=pt-br"
+    );
+    for (const genero of data.genres) {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=cdd74c9f441352e6a6aee5e2cc6532e6&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genero.id}&with_watch_monetization_types=flatrate`
+      );
+      this.filmesPorGenero.push({
+        id: genero.id,
+        name: genero.name,
+        movies: data.results,
+      });
+    }
+    console.log(this.filmesPorGenero);
+    this.generos = data.genres;
+  },
+  methods: {
+    async getMoviesByGenre(genre) {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=cdd74c9f441352e6a6aee5e2cc6532e6&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genre}&with_watch_monetization_types=flatrate`
+      );
+      return data.results;
+    },
   },
 };
 </script>
 <template>
   <div class="">
-    <h1 class="flex justify-center text-white text-4xl pb-7 ">Ação</h1>
-    <div class="flex justify-center text-white">
-      <CardComp v-for="movie of movies" :key="movie.id" :filme="movie" />
-    </div>
-    <h1 class="flex justify-center text-white text-4xl p-10">Aventura</h1>
-    <div class="flex justify-center text-white">
-      <CardComp v-for="movie of movies" :key="movie.id" :filme="movie" />
-    </div>
-    <h1 class="flex justify-center text-white text-4xl p-10">Romance</h1>
-    <div class="flex justify-center text-white">
-      <CardComp v-for="movie of movies" :key="movie.id" :filme="movie" />
-    </div>
-    <h1 class="flex justify-center text-white text-4xl p-10">Terror </h1>
-    <div class="flex justify-center text-white">
-      <CardComp v-for="movie of movies" :key="movie.id" :filme="movie" />
+    <div v-for="genero of generos" :key="genero.id">
+      <h1 class="flex justify-center text-white text-4xl pb-7">
+        {{ genero.name }}
+      </h1>
+      <div class="flex justify-center text-white">
+        <CardComp
+          v-for="movie of filmesPorGenero
+            .find((f) => f.id === genero.id)
+            .movies.slice(0, 5)"
+          :key="movie.id"
+          :filme="movie"
+        />
+      </div>
     </div>
   </div>
 </template>
